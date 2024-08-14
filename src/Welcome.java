39f9af15cee53a5838a7bd118070e616cf1f9d71
khavy <23632971.vy@student.iuh.edu.vn>
2024-08-14 16:04:19 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Pham Kha Vy!");
+        System.out.println("Welcome to Pham Kha Vy!");
+    }
+}
